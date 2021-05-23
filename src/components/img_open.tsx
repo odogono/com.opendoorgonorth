0db3acd50a5716b7b3169f0d5da54c1680c853d2
Alex Veenendaal <alex@opendoorgonorth.com>
2021-05-23 17:01:29 +0100

    grosmont project page

--- src/components/img_open.tsx
+++ src/components/img_open.tsx
@@ -3,6 +3,10 @@ import { resolveUrl } from '@site';
 
 export default function ImgOpen({src, ...props}){
 
+    if( src === undefined ){
+        return null;
+    }
+    
     let url = resolveUrl(src);
 
     return <a href={url}><img {...props} src={url} /></a>;
